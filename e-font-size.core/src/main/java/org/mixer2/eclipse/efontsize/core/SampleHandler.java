ec592c66c9c5818612c9fdcd40cb60678ae57c30
nabedge <nabedge@gmail.com>
2015-04-18 14:32:47 +0900

    add icon

--- e-font-size.core/src/main/java/org/mixer2/eclipse/efontsize/core/SampleHandler.java
+++ e-font-size.core/src/main/java/org/mixer2/eclipse/efontsize/core/SampleHandler.java
@@ -28,7 +28,8 @@ public class SampleHandler extends AbstractHandler {
      */
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        MessageDialog.openInformation(window.getShell(), "Eclipse Plugin Archetype", "Hello, Maven+Eclipse world,\n e-font-size is built with Tycho");
+        MessageDialog.openInformation(window.getShell(), 
+                "Eclipse Plugin Archetype", "Hello, Maven+Eclipse world,\n e-font-size is built with Tycho");
         return null;
     }
 
